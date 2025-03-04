a1168802c6321ac119236c3f92cd8952b254c309
CarlosPavajeau <cantte098@gmail.com>
2025-03-04 10:44:07 -0500

    feat: add DateTimeToUtcConverter on db context
    specify date time kind to Utc

--- Cetus/Infrastructure/Persistence/EntityFramework/CetusDbContext.cs
+++ Cetus/Infrastructure/Persistence/EntityFramework/CetusDbContext.cs
@@ -1,5 +1,7 @@
+using System.Linq.Expressions;
 using Cetus.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Cetus.Infrastructure.Persistence.EntityFramework;
 
@@ -11,4 +13,20 @@ public class CetusDbContext(DbContextOptions<CetusDbContext> options)
     public DbSet<Customer> Customers { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<OrderItem> OrderItems { get; set; }
+    
+    private class DateTimeToUtcConverter() : ValueConverter<DateTime, DateTime>(Serialize, Deserialize)
+    {
+        static Expression<Func<DateTime, DateTime>> Deserialize = 
+            x => x.Kind == DateTimeKind.Unspecified ? DateTime.SpecifyKind(x, DateTimeKind.Utc) : x;
+        static Expression<Func<DateTime, DateTime>> Serialize = x => x;
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        base.ConfigureConventions(configurationBuilder);
+        
+        configurationBuilder
+            .Properties<DateTime>()
+            .HaveConversion<DateTimeToUtcConverter>();
+    }
 }
