b374a1a36ce9569b377a8d7ee326aaaf9c895aca
cpavajea <cpavajea@emeal.nttdata.com>
2025-03-25 21:44:56 -0500

    feat: add update product command validator

---
+++ Cetus/Products/Application/Update/UpdateProductCommandValidator.cs
@@ -0,0 +1,38 @@
+using FluentValidation;
+
+namespace Cetus.Products.Application.Update;
+
+public sealed class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("El identificador del producto es requerido.");
+
+        RuleFor(x => x.Name)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("El nombre del producto es requerido.");
+
+        RuleFor(x => x.Price)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("El precio del producto es requerido.")
+            .GreaterThan(0)
+            .WithMessage("El precio del producto debe ser mayor a 0.");
+
+        RuleFor(x => x.Stock)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("La cantidad en stock del producto es requerida.")
+            .GreaterThan(0)
+            .WithMessage("La cantidad en stock del producto debe ser mayor a 0.");
+
+        RuleFor(x => x.ImageUrl)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("La URL de la imagen del producto es requerida.");
+    }
+}
