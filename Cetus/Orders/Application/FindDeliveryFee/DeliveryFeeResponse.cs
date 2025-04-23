e6825a59836e912d48dfdea0aa43258cdbce721f
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-04-23 07:59:07 -0500

    feat: update default delivery fee in DeliveryFeeResponse to 15000

--- Cetus/Orders/Application/FindDeliveryFee/DeliveryFeeResponse.cs
+++ Cetus/Orders/Application/FindDeliveryFee/DeliveryFeeResponse.cs
@@ -2,6 +2,6 @@ namespace Cetus.Orders.Application.FindDeliveryFee;
 
 public record DeliveryFeeResponse(Guid Id, decimal Fee)
 {
-    private const decimal DefaultFee = 10000.0M;
+    private const decimal DefaultFee = 15000.0M;
     public static readonly DeliveryFeeResponse Empty = new(Guid.Empty, DefaultFee);
 }
