32a0365c58ac1cf5e799497bfbacdfaa38c70be1
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-05-22 07:43:46 -0500

    feat: add Create endpoint for categories with command handling

---
+++ Cetus.Api/Endpoints/Categories/Create.cs
@@ -0,0 +1,21 @@
+using Application.Abstractions.Messaging;
+using Application.Categories.Create;
+using Cetus.Api.Extensions;
+using Cetus.Api.Infrastructure;
+
+namespace Cetus.Api.Endpoints.Categories;
+
+internal sealed class Create : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("categories",
+            async (CreateCategoryCommand command, ICommandHandler<CreateCategoryCommand, bool> handler,
+                CancellationToken cancellationToken) =>
+            {
+                var result = await handler.Handle(command, cancellationToken);
+
+                return result.Match(Results.Ok, CustomResults.Problem);
+            });
+    }
+}
