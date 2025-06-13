c113ea53cc411cfd0f4d0cfd8756199dbb87ddda
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-06-13 11:03:24 -0500

    feat: remove unnecessary validation attributes from OrderItem class

--- Domain/Orders/OrderItem.cs
+++ Domain/Orders/OrderItem.cs
@@ -1,19 +1,17 @@
-using System.ComponentModel.DataAnnotations;
 using Domain.Products;
 
 namespace Domain.Orders;
 
 public sealed class OrderItem
 {
-    [Required] [Key] public Guid Id { get; set; }
+    public Guid Id { get; set; }
 
-    [Required] [MaxLength(256)] public string ProductName { get; set; } = string.Empty;
-
-    [MaxLength(512)] public string? ImageUrl { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
 
     public int Quantity { get; set; }
     public decimal Price { get; set; }
 
-    [Required] public Guid ProductId { get; set; }
+    public Guid ProductId { get; set; }
     public Product? Product { get; set; }
 }
