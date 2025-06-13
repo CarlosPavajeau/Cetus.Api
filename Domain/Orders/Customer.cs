be421c3a7b7ca5b19c30d458114bd63a2fd8ff4e
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-06-13 11:00:14 -0500

    feat: simplify validation attributes in Customer class

--- Domain/Orders/Customer.cs
+++ Domain/Orders/Customer.cs
@@ -4,18 +4,13 @@ namespace Domain.Orders;
 
 public sealed class Customer
 {
-    [Required] [MaxLength(50)] [Key] public string Id { get; set; } = string.Empty;
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
 
-    [Required] [MaxLength(256)] public string Name { get; set; } = string.Empty;
+    [EmailAddress] public string Email { get; set; } = string.Empty;
 
-    [Required]
-    [MaxLength(256)]
-    [EmailAddress]
-    public string Email { get; set; } = string.Empty;
-
-    [Required] [MaxLength(256)] public string Phone { get; set; } = string.Empty;
-
-    [Required] [MaxLength(256)] public string Address { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
