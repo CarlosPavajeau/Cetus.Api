7608d9d54d85c1f84cc3d9db4601792873620a48
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-06-13 11:03:16 -0500

    feat: streamline OrderItem properties and add configuration class

---
+++ Infrastructure/Orders/OrderItemConfiguration.cs
@@ -0,0 +1,29 @@
+using Domain.Orders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Orders;
+
+internal sealed class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.HasKey(oi => oi.Id);
+
+        builder.Property(oi => oi.ProductName)
+            .IsRequired()
+            .HasMaxLength(256);
+
+        builder.Property(oi => oi.ImageUrl)
+            .HasMaxLength(512);
+
+        builder.Property(oi => oi.Quantity)
+            .IsRequired();
+
+        builder.Property(oi => oi.Price)
+            .IsRequired();
+
+        builder.Property(oi => oi.ProductId)
+            .IsRequired();
+    }
+}
