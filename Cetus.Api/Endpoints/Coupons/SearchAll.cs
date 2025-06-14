14ea3fbacbd709b256fb372237bca40b7331289d
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-06-14 18:03:12 -0500

    feat: implement caching for search all coupons endpoint

--- Cetus.Api/Endpoints/Coupons/SearchAll.cs
+++ Cetus.Api/Endpoints/Coupons/SearchAll.cs
@@ -3,6 +3,7 @@ using Application.Coupons;
 using Application.Coupons.SearchAll;
 using Cetus.Api.Extensions;
 using Cetus.Api.Infrastructure;
+using Microsoft.Extensions.Caching.Hybrid;
 
 namespace Cetus.Api.Endpoints.Coupons;
 
@@ -11,11 +12,17 @@ internal sealed class SearchAll : IEndpoint
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
         app.MapGet("coupons", async (
+            HybridCache cache,
             IQueryHandler<SearchAllCouponsQuery, IEnumerable<CouponResponse>> handler,
             CancellationToken cancellationToken) =>
         {
             var query = new SearchAllCouponsQuery();
-            var result = await handler.Handle(query, cancellationToken);
+
+            var result = await cache.GetOrCreateAsync(
+                "coupons",
+                async token => await handler.Handle(query, token),
+                cancellationToken: cancellationToken
+            );
 
             return result.Match(Results.Ok, CustomResults.Problem);
         }).WithTags(Tags.Coupons).HasPermission(ClerkPermissions.AppAccess);
