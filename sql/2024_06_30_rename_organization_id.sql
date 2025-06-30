5723530e387cc6862033540455f94768973275bc
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-06-30 11:16:10 -0500

    feat: rename organization_id to store_id in categories, products, and orders tables

---
+++ sql/2024_06_30_rename_organization_id.sql
@@ -0,0 +1,24 @@
+BEGIN;
+
+ALTER TABLE categories
+DROP
+COLUMN organization_id;
+
+ALTER TABLE products
+DROP
+COLUMN organization_id;
+
+ALTER TABLE orders
+DROP
+COLUMN organization_id;
+
+ALTER TABLE categories
+    ADD COLUMN store_id UUID;
+
+ALTER TABLE products
+    ADD COLUMN store_id UUID;
+
+ALTER TABLE orders
+    ADD COLUMN store_id UUID;
+
+COMMIT;
