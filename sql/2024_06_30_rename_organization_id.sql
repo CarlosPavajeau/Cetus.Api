cd11a41f40f4f34136f887e135ad39df79a69b8e
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-06-30 11:21:09 -0500

    feat: rename organization_id to store_id in delivery_fees table

--- sql/2024_06_30_rename_organization_id.sql
+++ sql/2024_06_30_rename_organization_id.sql
@@ -12,6 +12,10 @@ ALTER TABLE orders
 DROP
 COLUMN organization_id;
 
+ALTER TABLE delivery_fees
+DROP
+COLUMN organization_id;
+
 ALTER TABLE categories
     ADD COLUMN store_id UUID;
 
@@ -19,6 +23,9 @@ ALTER TABLE products
     ADD COLUMN store_id UUID;
 
 ALTER TABLE orders
+    ADD COLUMN store_id UUID;
+
+ALTER TABLE delivery_fees
     ADD COLUMN store_id UUID;
 
 COMMIT;
