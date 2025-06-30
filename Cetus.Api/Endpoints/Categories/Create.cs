59bd3c4e9a41d4e60c71fafb39fbd2727cf7de12
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-06-30 13:09:20 -0500

    feat: implement tenant-specific cache invalidation in CreateCategory endpoint

--- Cetus.Api/Endpoints/Categories/Create.cs
+++ Cetus.Api/Endpoints/Categories/Create.cs
@@ -1,7 +1,9 @@
+using Application.Abstractions.Data;
 using Application.Abstractions.Messaging;
 using Application.Categories.Create;
 using Cetus.Api.Extensions;
 using Cetus.Api.Infrastructure;
+using Microsoft.Extensions.Caching.Hybrid;
 
 namespace Cetus.Api.Endpoints.Categories;
 
@@ -12,9 +14,16 @@ internal sealed class Create : IEndpoint
         app.MapPost("categories", async (
             CreateCategoryCommand command,
             ICommandHandler<CreateCategoryCommand, bool> handler,
+            HybridCache cache,
+            ITenantContext context,
             CancellationToken cancellationToken) =>
         {
             var result = await handler.Handle(command, cancellationToken);
+
+            if (result.IsSuccess)
+            {
+                await cache.RemoveAsync($"categories-{context.Id}", cancellationToken);
+            }
 
             return result.Match(Results.Ok, CustomResults.Problem);
         }).WithTags(Tags.Categories).HasPermission(ClerkPermissions.AppAccess);
