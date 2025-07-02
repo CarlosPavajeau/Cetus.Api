4cdcaccf395efb6e8bb5d33e4b2171b43509c896
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-07-02 17:41:50 -0500

    feat: include more data on store response

--- Application/Stores/StoreResponse.cs
+++ Application/Stores/StoreResponse.cs
@@ -2,10 +2,16 @@ using Domain.Stores;
 
 namespace Application.Stores;
 
-public sealed record StoreResponse(Guid Id, string Name, string? CustomDomain)
+public sealed record StoreResponse(Guid Id, string Name, string Slug, string? CustomDomain, string? LogoUrl)
 {
     public static StoreResponse FromStore(Store store)
     {
-        return new StoreResponse(store.Id, store.Name, store.CustomDomain);
+        return new StoreResponse(
+            store.Id,
+            store.Name,
+            store.Slug,
+            store.CustomDomain,
+            store.LogoUrl
+        );
     }
 }
