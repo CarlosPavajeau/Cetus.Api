30588a4a58b4a62d4dbb0e6c5feef27fd2fc3186
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-07-27 09:32:02 -0500

    feat(mercadopago): add endpoint for generating MercadoPago authorization URL

--- Cetus.Api/Endpoints/Stores/GenerateMercadoPagoAuthorizationUrl.cs
+++ Cetus.Api/Endpoints/Stores/GenerateMercadoPagoAuthorizationUrl.cs
@@ -1,18 +1,18 @@
 using Application.Abstractions.MercadoPago;
 
-namespace Cetus.Api.Endpoints.MercadoPago;
+namespace Cetus.Api.Endpoints.Stores;
 
-internal sealed class GenerateAuthorizationUrl : IEndpoint
+internal sealed class GenerateMercadoPagoAuthorizationUrl : IEndpoint
 {
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapGet("mercadopago/authorization-url", async (
+        app.MapGet("stores/payment-providers/mercado-pago/authorization-url", async (
             IMercadoPagoClient mercadoPagoClient,
             CancellationToken cancellationToken
         ) =>
         {
             var authorizationUrl = await mercadoPagoClient.GenerateAuthorizationUrl(cancellationToken);
             return Results.Ok(authorizationUrl);
-        }).WithTags(Tags.MercadoPago);
+        }).WithTags(Tags.Stores, Tags.MercadoPago);
     }
 }
