31ea88cef01f63907635dd04b9aaeb361688cc39
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-08-18 18:06:24 -0500

    refactor: add sealed keyword

--- Application/Products/Options/CreateType/CreateProductOptionTypeCommand.cs
+++ Application/Products/Options/CreateType/CreateProductOptionTypeCommand.cs
@@ -2,4 +2,4 @@ using Application.Abstractions.Messaging;
 
 namespace Application.Products.Options.CreateType;
 
-public record CreateProductOptionTypeCommand(string Name, string[] Values) : ICommand;
+public sealed record CreateProductOptionTypeCommand(string Name, string[] Values) : ICommand;
