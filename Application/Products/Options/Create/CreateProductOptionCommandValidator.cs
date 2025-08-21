19cbf985dd3bad16f955295f4e1f8224937c2b54
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-08-21 12:37:50 -0500

    feat: add validator for CreateProductOptionCommand to enforce product and option type requirements

---
+++ Application/Products/Options/Create/CreateProductOptionCommandValidator.cs
@@ -0,0 +1,18 @@
+using FluentValidation;
+
+namespace Application.Products.Options.Create;
+
+public sealed class CreateProductOptionCommandValidator : AbstractValidator<CreateProductOptionCommand>
+{
+    public CreateProductOptionCommandValidator()
+    {
+        RuleFor(x => x.ProductId)
+            .NotEmpty()
+            .WithMessage("El id del producto es requerido");
+
+        RuleFor(x => x.OptionTypeId)
+            .NotEmpty()
+            .GreaterThan(0)
+            .WithMessage("La opción es requerida");
+    }
+}
