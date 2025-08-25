7ccf250fc68ef96e8ab0cbab00ee28e57666e737
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-08-24 23:23:05 -0500

    feat: add variant_id to order_items and update foreign key constraints

---
+++ sql/2025_08_24_add_variant_id_to_order_items.sql
@@ -0,0 +1,19 @@
+ALTER TABLE order_items
+    ADD COLUMN variant_id BIGINT;
+
+UPDATE
+    order_items oi
+SET variant_id = pv.id
+FROM product_variants pv
+WHERE oi.product_id = pv.product_id;
+
+ALTER TABLE order_items
+    ALTER COLUMN variant_id SET NOT NULL;
+
+ALTER TABLE order_items
+    ADD CONSTRAINT fk_order_item_variant
+        FOREIGN KEY (variant_id)
+            REFERENCES product_variants (id);
+
+ALTER TABLE order_items
+    DROP COLUMN product_id;
