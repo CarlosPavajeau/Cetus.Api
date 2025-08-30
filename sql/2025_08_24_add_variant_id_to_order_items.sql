d685e9c175709307544eea7403fa2b3388e7c3af
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-08-30 10:07:43 -0500

    feat: update foreign key constraint for variant_id in order_items and create index

--- sql/2025_08_24_add_variant_id_to_order_items.sql
+++ sql/2025_08_24_add_variant_id_to_order_items.sql
@@ -13,7 +13,10 @@ ALTER TABLE order_items
 ALTER TABLE order_items
     ADD CONSTRAINT fk_order_item_variant
         FOREIGN KEY (variant_id)
-            REFERENCES product_variants (id);
+            REFERENCES product_variants (id)
+            ON DELETE SET NULL;
+
+CREATE INDEX IF NOT EXISTS idx_order_items_variant_id ON order_items (variant_id);
 
 ALTER TABLE order_items
     DROP COLUMN product_id;
