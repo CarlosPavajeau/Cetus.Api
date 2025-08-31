41cda5b70a7d147a183ca07d569ccc5e2d595218
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-08-31 17:59:51 -0500

    feat: remove price, stock, and images validation from product command validators

--- Application/Products/Update/UpdateProductCommandValidator.cs
+++ Application/Products/Update/UpdateProductCommandValidator.cs
@@ -15,29 +15,5 @@ public sealed class UpdateProductCommandValidator : AbstractValidator<UpdateProd
             .NotNull()
             .NotEmpty()
             .WithMessage("El nombre del producto es requerido.");
-
-        RuleFor(x => x.Price)
-            .NotNull()
-            .NotEmpty()
-            .WithMessage("El precio del producto es requerido.")
-            .GreaterThan(0)
-            .WithMessage("El precio del producto debe ser mayor a 0.");
-
-        RuleFor(x => x.Stock)
-            .NotNull()
-            .NotEmpty()
-            .WithMessage("La cantidad en stock del producto es requerida.")
-            .GreaterThan(0)
-            .WithMessage("La cantidad en stock del producto debe ser mayor a 0.");
-        
-        RuleFor(x => x.CategoryId)
-            .NotNull()
-            .NotEmpty()
-            .WithMessage("El identificador de la categoría del producto es requerido.");
-
-        RuleFor(x => x.Images)
-            .NotNull()
-            .NotEmpty()
-            .WithMessage("Las imágenes del producto son requeridas");
     }
 }
