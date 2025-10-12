6e45c855dae59065a7d7ff588583e929e655df13
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-10-12 18:26:48 -0500

    refactor: remove UpdateOrderCommand

--- Application/Orders/Update/UpdateOrderCommand.cs
+++
@@ -1,8 +0,0 @@
-using Application.Abstractions.Messaging;
-using Application.Orders.SearchAll;
-using Domain.Orders;
-
-namespace Application.Orders.Update;
-
-public sealed record UpdateOrderCommand(Guid Id, OrderStatus Status, string? TransactionId = null)
-    : ICommand<OrderResponse>;
