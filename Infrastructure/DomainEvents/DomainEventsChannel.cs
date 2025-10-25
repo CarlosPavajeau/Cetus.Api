a636ce358e4eacf8615193fe9ec2794fe13d0636
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-10-24 22:55:38 -0500

    feat: change domain events channel to use bounded options for improved performance

--- Infrastructure/DomainEvents/DomainEventsChannel.cs
+++ Infrastructure/DomainEvents/DomainEventsChannel.cs
@@ -5,7 +5,15 @@ namespace Infrastructure.DomainEvents;
 
 internal sealed class DomainEventsChannel
 {
-    private readonly Channel<IDomainEvent> _channel = Channel.CreateUnbounded<IDomainEvent>();
+    private static readonly BoundedChannelOptions DefaultOptions = new(capacity: 10_000)
+    {
+        SingleReader = true,
+        SingleWriter = false,
+        FullMode = BoundedChannelFullMode.Wait,
+        AllowSynchronousContinuations = true
+    };
+
+    private readonly Channel<IDomainEvent> _channel = Channel.CreateBounded<IDomainEvent>(DefaultOptions);
 
     public ValueTask Publish(IDomainEvent domainEvent, CancellationToken cancellationToken = default)
     {
