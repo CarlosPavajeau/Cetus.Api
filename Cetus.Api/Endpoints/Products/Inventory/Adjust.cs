5e8ba7467a1b0def07d98856bf1ad63d7296c96e
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-12-14 17:22:02 -0500

    feat: implement cache invalidation for successful inventory adjustments

--- Cetus.Api/Endpoints/Products/Inventory/Adjust.cs
+++ Cetus.Api/Endpoints/Products/Inventory/Adjust.cs
@@ -1,7 +1,9 @@
+using Application.Abstractions.Data;
 using Application.Abstractions.Messaging;
 using Application.Products.Inventory.Adjust;
 using Cetus.Api.Extensions;
 using Cetus.Api.Infrastructure;
+using Microsoft.Extensions.Caching.Hybrid;
 
 namespace Cetus.Api.Endpoints.Products.Inventory;
 
@@ -12,9 +14,16 @@ internal sealed class Adjust : IEndpoint
         app.MapPost("inventory/adjustments", async (
             AdjustInventoryStockCommand command,
             ICommandHandler<AdjustInventoryStockCommand> handler,
+            ITenantContext tenant,
+            HybridCache cache,
             CancellationToken cancellationToken) =>
         {
             var result = await handler.Handle(command, cancellationToken);
+
+            if (result.IsSuccess)
+            {
+                await cache.RemoveByTagAsync(["products", $"tenant-{tenant.Id}"], cancellationToken);
+            }
 
             return result.Match(Results.NoContent, CustomResults.Problem);
         }).WithTags(Tags.Inventory);
