570debec351fe8db2ab524c3cffa70ce07d26f52
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-01-22 11:57:54 -0500

    feat: add pagination and filtering options to SearchAllProductsForSaleQuery

--- Application/Products/SearchForSale/SearchAllProductsForSaleQuery.cs
+++ Application/Products/SearchForSale/SearchAllProductsForSaleQuery.cs
@@ -2,4 +2,9 @@ using Application.Abstractions.Messaging;
 
 namespace Application.Products.SearchForSale;
 
-public sealed record SearchAllProductsForSaleQuery : IQuery<IEnumerable<SimpleProductForSaleResponse>>;
+public sealed record SearchAllProductsForSaleQuery(
+    int Page = 1,
+    int PageSize = 20,
+    IReadOnlyList<Guid>? CategoryIds = null,
+    string? SearchTerm = null
+) : IQuery<IEnumerable<SimpleProductForSaleResponse>>;
