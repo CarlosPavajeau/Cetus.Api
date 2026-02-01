f23998ec273dcbb624a66d4c383477352a49edaa
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-02-01 16:06:15 -0500

    feat: make DocumentType property nullable in Customer for improved flexibility

--- Domain/Orders/Customer.cs
+++ Domain/Orders/Customer.cs
@@ -6,7 +6,7 @@ public sealed class Customer
 {
     public Guid Id { get; set; }
 
-    public DocumentType DocumentType { get; set; }
+    public DocumentType? DocumentType { get; set; }
     public string? DocumentNumber { get; set; }
 
     public string Name { get; set; } = string.Empty;
