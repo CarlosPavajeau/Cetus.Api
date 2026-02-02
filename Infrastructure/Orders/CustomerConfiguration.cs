85da003ed8ec3b31ce90d29791e25fd851d2d4a0
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-02-01 19:03:22 -0500

    feat: remove auto-generation of Id in CustomerConfiguration

--- Infrastructure/Orders/CustomerConfiguration.cs
+++ Infrastructure/Orders/CustomerConfiguration.cs
@@ -10,9 +10,6 @@ internal sealed class CustomerConfiguration : IEntityTypeConfiguration<Customer>
     {
         builder.HasKey(c => c.Id);
 
-        builder.Property(c => c.Id)
-            .ValueGeneratedOnAdd();
-
         builder.Property(c => c.Name)
             .IsRequired()
             .HasMaxLength(256);
