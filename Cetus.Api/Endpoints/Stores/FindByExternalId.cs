9b9a98903b92248d6c2d28722a9d083f0a7a13f8
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-02-07 18:22:45 -0500

    feat: implement caching for store retrieval by external ID to improve performance

--- Cetus.Api/Endpoints/Stores/FindByExternalId.cs
+++ Cetus.Api/Endpoints/Stores/FindByExternalId.cs
@@ -3,6 +3,7 @@ using Application.Stores;
 using Application.Stores.FindByExternalId;
 using Cetus.Api.Extensions;
 using Cetus.Api.Infrastructure;
+using Microsoft.Extensions.Caching.Hybrid;
 
 namespace Cetus.Api.Endpoints.Stores;
 
@@ -13,10 +14,22 @@ internal sealed class FindByExternalId : IEndpoint
         app.MapGet("stores/by-external-id/{externalId}", async (
             string externalId,
             IQueryHandler<FindStoreByExternalId, StoreResponse> handler,
+            HybridCache cache,
             CancellationToken cancellationToken) =>
         {
             var query = new FindStoreByExternalId(externalId);
-            var result = await handler.Handle(query, cancellationToken);
+            string cacheKey = $"store:by-external-id:${externalId}";
+
+            var result = await cache.GetOrCreateAsync(
+                cacheKey,
+                async token => await handler.Handle(query, token),
+                new HybridCacheEntryOptions
+                {
+                    Expiration = TimeSpan.FromMinutes(15),
+                    LocalCacheExpiration = TimeSpan.FromMinutes(5)
+                },
+                cancellationToken: cancellationToken
+            );
 
             return result.Match(Results.Ok, CustomResults.Problem);
         }).WithTags(Tags.Stores);
