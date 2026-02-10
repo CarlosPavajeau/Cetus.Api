eec5290f90c72ed39d1a116cc14de63554364bc5
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-02-09 22:51:59 -0500

    feat: add Token and Status properties to PaymentLink with unique index

--- Infrastructure/PaymentLinks/PaymentLinkConfiguration.cs
+++ Infrastructure/PaymentLinks/PaymentLinkConfiguration.cs
@@ -10,6 +10,18 @@ internal sealed class PaymentLinkConfiguration : IEntityTypeConfiguration<Paymen
     {
         builder.HasKey(pl => pl.Id);
 
+        builder.Property(pl => pl.Token).HasMaxLength(64).IsRequired();
+        builder.HasIndex(pl => pl.Token).IsUnique();
+
+        builder.Property(pl => pl.Status)
+            .IsRequired();
+
+        builder.Property(pl => pl.ExpiresAt)
+            .IsRequired();
+
+        builder.Property(pl => pl.CreatedAt)
+            .IsRequired();
+
         builder.HasOne(pl => pl.Order)
             .WithMany()
             .HasForeignKey(pl => pl.OrderId);
