92a621ead48b5187fd33df1f2cc6e5811be56f40
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-02-12 11:10:59 -0500

    feat: add JsonStringEnumMemberName attribute for enum serialization

--- Domain/Orders/DocumentType.cs
+++ Domain/Orders/DocumentType.cs
@@ -1,12 +1,22 @@
+using System.Text.Json.Serialization;
 using NpgsqlTypes;
 
 namespace Domain.Orders;
 
 public enum DocumentType
 {
-    [PgName("CC")] CC,
-    [PgName("CE")] CE,
-    [PgName("NIT")] NIT,
-    [PgName("PP")] PP,
-    [PgName("OTHER")] OTHER
+    [PgName("CC"), JsonStringEnumMemberName("CC")]
+    CC,
+
+    [PgName("CE"), JsonStringEnumMemberName("CE")]
+    CE,
+
+    [PgName("NIT"), JsonStringEnumMemberName("NIT")]
+    NIT,
+
+    [PgName("PP"), JsonStringEnumMemberName("PP")]
+    PP,
+
+    [PgName("OTHER"), JsonStringEnumMemberName("OTHER")]
+    OTHER
 }
