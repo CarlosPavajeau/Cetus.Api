712eec345e8353eb414c9afbdf64f62c27d1adb5
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-02-16 11:24:39 -0500

    feat: update constraint for compare-at price to ensure it is greater than the retail price

--- sql/2026_02_16_00_add_product_variant_prices.sql
+++ sql/2026_02_16_00_add_product_variant_prices.sql
@@ -5,4 +5,4 @@ ALTER TABLE product_variants
 ALTER TABLE product_variants
     ADD CONSTRAINT chk_variant_compare_at_price_nonnegative CHECK (compare_at_price >= 0),
     ADD CONSTRAINT chk_variant_retail_price_nonnegative CHECK (retail_price >= 0),
-    ADD CONSTRAINT chk_variant_compare_at_price_greater_than_price CHECK (compare_at_price >= price);
+    ADD CONSTRAINT chk_variant_compare_at_price_greater_than_price CHECK (compare_at_price > price);
